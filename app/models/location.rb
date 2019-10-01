8498d3c8faa5993a1aa032ce2bc77deebd6c2aec
Raphael <raphaelndauwa@gmail.com>
2019-10-01 17:43:17 +0300

    complete the applicatio where one can submit location by submitting address

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ApplicationRecord
   geocoded_by :address       # can also be an IP address
-  after_validation :geocode  # auto-fetch coordinates
+  after_validation :geocode
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191001113400) do
+ActiveRecord::Schema.define(version: 20191001135027) do
 
   create_table "downloads", force: :cascade do |t|
     t.string "ip"
