ee543e40c2c1c38c89ce4b3ce908e1992c00b1d4
Raphael Karanja <raphaelndauwa@gmail.com>
2019-10-01 14:46:42 +0300

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191001112203) do
+ActiveRecord::Schema.define(version: 20191001113400) do
+
+  create_table "downloads", force: :cascade do |t|
+    t.string "ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "address"
